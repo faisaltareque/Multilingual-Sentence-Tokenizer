79397ef82815368a2edc9b8b0e176e6abcaed00d
Faisal Tareque <faisaltareque98@gmail.com>
2022-08-12 19:38:11 +0600

    Update NLTK Support

--- src/multilingual_sentence_tokenizer/library/nltk_support.py
+++ src/multilingual_sentence_tokenizer/library/nltk_support.py
@@ -2,5 +2,4 @@ import nltk
 nltk.download('punkt')
 from nltk.tokenize import sent_tokenize
 def sentence_tokenize(text:str, lang:str):
-    print(sent_tokenize(text))
     return sent_tokenize(text)
